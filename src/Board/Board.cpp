ced27e7bc9cf3cdfb24ced980ab6960bb115fb0b
Yurevych Nataliia <116509687+yurevych2@users.noreply.github.com>
2023-06-11 15:10:32 +0300

    Fix the order of the figures at the board

--- src/Board/Board.cpp
+++ src/Board/Board.cpp
@@ -4,39 +4,41 @@
 #include "Knight.h"
 #include "Queen.h"
 #include "Rook.h"
+#include "King.h"
+#include "Pawn.h"
 
 
 Board::Board() {
     // Fill the chessboard with some initial values
 
     // Initialize the white figures
-    board[0][0] = new Rook(1, 0, true);; // Rook
+    board[0][0] = new Rook(1, 0, true);
     board[0][1] = new Knight(0, 1, true);
     board[0][2] = new Bishop(0, 2, true);
-    board[0][3] = new Bishop(0, 4, true);  // King
+    board[0][3] = new King(0, 4, true);
     board[0][4] = new Queen(0, 3, true);
     board[0][5] = new Bishop(0, 5, true);
     board[0][6] = new Knight(0, 6, true);
-    board[0][7] = new Rook(0, 7, true);  // Rook
+    board[0][7] = new Rook(0, 7, true);
 
     // Initialize the white pawns
     for (int col = 0; col < 8; col++) {
-        board[1][col] = new Bishop(1, col, true);  // Pawn
+        board[1][col] = new Pawn(1, col, true);
     }
 
     // Initialize the black figures
-    board[7][0] = new Rook(7, 0, false);  // Rook
+    board[7][0] = new Rook(7, 0, false);
     board[7][1] = new Knight(7, 1, false);
     board[7][2] = new Bishop(7, 2, false);
-    board[7][3] = new Bishop(7, 4, false);  // King
+    board[7][3] = new King(7, 4, false);
     board[7][4] = new Queen(7, 3, false);
     board[7][5] = new Bishop(7, 5, false);
     board[7][6] = new Knight(7, 6, false);
-    board[7][7] = new Rook(7, 7, false);  // Rook
+    board[7][7] = new Rook(7, 7, false);
 
     // Initialize the black pawns
     for (int col = 0; col < 8; col++) {
-        board[6][col] = new Bishop(6, col, false);  // Pawn
+        board[6][col] = new Pawn(6, col, false);
     }
 
     // Initialize the empty positions
@@ -46,8 +48,6 @@ Board::Board() {
         }
     }
 
-    // temporary
-    board[4][4] = new Bishop(4, 4, true);
 }
 
 // N: Do we still need it?
